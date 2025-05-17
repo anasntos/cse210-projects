6afca5000aa4d56dad6b53667e4f34e9cb98d847
anasntos <gabrielysantosm@gmail.com>
2025-05-17 00:23:38 -0300

    Update Job.cs

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -1,1 +1,15 @@
+using System;
+
+public class Job
+{
+    public string _jobTitles;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine ($"{_jobTitles} ({_company}) {_startYear}-{_endYear}");
+    }
+}
 
